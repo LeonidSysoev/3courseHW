311c580e477742ce2772f07b1b87d78a5825839f
LeonidHome <defenser2205@gmail.com>
2023-07-24 00:03:11 +0300

    task 4.1

--- src/main/java/ru/hogwarts/school/repositories/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/AvatarRepository.java
@@ -1,11 +1,17 @@
 package ru.hogwarts.school.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.hogwarts.school.model.Avatar;
 
 import java.util.Optional;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
 
+
+
     Optional<Avatar> findByStudentId(Long id);
 }
+
--- src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
@@ -2,14 +2,26 @@ package ru.hogwarts.school.repositories;
 
 import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Collection<Student> findAllByAge(int age);
 
     Collection<Student> findByAgeBetween(int minAge, int maxAge);
+
+    @Query(value = "SELECT COUNT(*) from student", nativeQuery = true)
+    int getAllQuantity();
+
+    @Query(value = "select avg(age) from student", nativeQuery = true)
+    long getAverageAge();
+
+    @Query(value = "select * from student order by id desc limit 5", nativeQuery = true)
+    List<Student> getLastFiveStudents();
+
 
 }
