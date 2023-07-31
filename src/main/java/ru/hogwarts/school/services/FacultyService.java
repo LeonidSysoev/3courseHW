7318473c0a2eb19d27c487ce72dc7af675d0a386
LeonidHome <defenser2205@gmail.com>
2023-07-31 17:36:21 +0300

    task 4.4.1

--- src/main/java/ru/hogwarts/school/services/FacultyService.java
+++ src/main/java/ru/hogwarts/school/services/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
+    private static final Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     private final FacultyRepository facultyRepository;
 
@@ -22,31 +25,38 @@ public class FacultyService {
     }
 
     public Faculty add(Faculty faculty) {
+        logger.info("Called method add with argument {}", faculty);
 
         return facultyRepository.save(faculty);
     }
 
     public Faculty edit(Faculty faculty) {
+        logger.info("Called method edit with argument {}", faculty);
         return facultyRepository.save(faculty);
     }
 
     public Faculty find(long id) {
+        logger.info("Called method find with argument", id);
         return facultyRepository.findById(id).get();
     }
 
     public void delete(long id) {
+        logger.info("Called method delete with argument {}", id);
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> findByColor(String color) {
+        logger.info("Called method findByColor with argument {}", color);
         return facultyRepository.findAllByColor(color);
     }
 
     public Collection<Faculty> findByNameOrColor(String name, String color) {
+        logger.info("Called method findByNameOrColor with argument name {}, color {}", name, color);
         return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(name, color);
     }
 
     public Collection<Student> findFacultyByStudent(Long id) {
+        logger.info("Called method findFacultyByStudent with argument {}", id);
         return facultyRepository.findById(id).map(Faculty::getStudents).orElse(Collections.emptyList());
     }
 }
